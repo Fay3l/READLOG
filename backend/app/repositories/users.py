6c2f22234e7349bb2ee85c5605fc3a8d4e7b393f
Fayel MOHAMED <mohamed.fayel@yahoo.com>
2026-09-17 02:23:12 +0200

    fix function get user

--- backend/app/repositories/users.py
+++ backend/app/repositories/users.py
@@ -30,7 +30,7 @@ async def verify_user(db: Session, name: str, email: str):
 
 
 async def get_user_by_name(name: str, db: Session):
-    res = db.query(Users).filter(Users.name == name).first()
+    res = db.query(Users).filter(Users.name == name | Users.email == name).first()
     if res is None:
         return None
 
